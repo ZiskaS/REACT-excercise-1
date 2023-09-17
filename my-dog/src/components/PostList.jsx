9cbf76b1795a0863daddbbedea4a2d37f024595e
¨ZiskaS¨ <franziska.stude@gmx.de>
2023-09-17 21:27:50 +0200

    update excercice 1

--- my-dog/src/components/PostList.jsx
+++ my-dog/src/components/PostList.jsx
@@ -1,88 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import Post from './Post';
+import React from 'react';
+import Post from './Post'; 
 
-function PostList({ searchValue }) {
-  const [isLoading, setIsLoading] = useState(true);
-  const [filteredPosts, setFilteredPosts] = useState([]);
-
-  useEffect(() => {
+function PostList() {
     const posts = [
       {
-        author: 'Elisa',
-        createdAt: new Date(),
-        text: 'I love my dog - so cute!',
-        image: 'dog-1.jpg',
-        comments: 22,
-        likes: 99,
+      author: 'Elisa',
+      date: new Date(), // Updated to use JavaScript Date object
+      description: 'I love my dog - so cute!',
+      image: 'dog-1.jpg',
+      comments: 22,
+      likes: 99,
       },
+  
       {
         author: 'Lucia',
-        createdAt: new Date(),
-        text: 'She is so sweet!',
+        date: new Date(), // Updated to use JavaScript Date object
+        description: 'I love my dog - so cute!',
         image: 'dog-2.jpg',
         comments: 5,
         likes: 10,
-      },
-      {
-        author: 'Ana',
-        createdAt: new Date(),
-        text: 'I adore my furry friend!',
-        image: 'dog-3.jpg',
-        comments: 15,
-        likes: 233,
-      },
+        },
+  
+        {
+          author: 'Ana',
+          date: new Date(), // Updated to use JavaScript Date object
+          description: 'I love my dog - so cute!',
+          image: 'dog-3.jpg',
+          comments: 15,
+          likes: 233,
+          }
     ];
-
-    const filterPosts = (query) => {
-      const lowercasedQuery = query.toLowerCase();
-      return posts.filter((post) => {
-        const lowercasedAuthor = post.author.toLowerCase();
-        const lowercasedText = post.text.toLowerCase();
-        return (
-          lowercasedAuthor.includes(lowercasedQuery) ||
-          lowercasedText.includes(lowercasedQuery)
-        );
-      });
-    };
-
-    const timer = setTimeout(() => {
-      const filtered = filterPosts(searchValue);
-      setFilteredPosts(filtered);
-      setIsLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, [searchValue]);
-
-  const handleLikeClick = (postIndex) => {
-    // Create a copy of the filteredPosts array
-    const updatedPosts = [...filteredPosts];
-    // Increment the like count for the specific post
-    updatedPosts[postIndex].likes += 1;
-    // Update the state with the new array
-    setFilteredPosts(updatedPosts);
-  };
-
-  return (
-    <div className="container">
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        filteredPosts.map((post, index) => (
+  
+    return (
+      <div className="container">
+        {posts.map((post) => (
           <Post
-            key={index}
             author={post.author}
-            createdAt={post.createdAt}
-            text={post.text}
+            date={post.date}
+            description={post.description}
             image={post.image}
             comments={post.comments}
             likes={post.likes}
-            onLikeClick={() => handleLikeClick(index)} // Pass the function to the Post component
           />
-        ))
-      )}
-    </div>
-  );
-}
-
+        ))}
+      </div>
+    );
+  }
+  
 export default PostList;
--- my-dog/src/components/SearchBar.jsx
+++ my-dog/src/components/SearchBar.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 
-function SearchBar({ value, onSearch }) {
-  const handleInputChange = (e) => {
-    const inputValue = e.target.value;
-    onSearch(inputValue);
-  };
+function SearchBar() {
+    return (
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
+      </div>
+    );
+  }
 
-  return (
-    <div className="input-group mb-3">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Search"
-        aria-label="Search"
-        aria-describedby="basic-addon2"
-        value={value}
-        onChange={handleInputChange}
-      />
-    </div>
-  );
-}
-
-export default SearchBar;
-
+  export default SearchBar;
