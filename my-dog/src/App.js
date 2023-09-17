73deb7d07c1b3fdd346d0be5a0b40a1aefcc2476
¨ZiskaS¨ <franziska.stude@gmx.de>
2023-09-17 21:01:13 +0200

    add login

--- my-dog/src/App.js
+++ my-dog/src/App.js
@@ -1,32 +1,60 @@
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import NavBar from './components/NavBar';
-import SearchBar from './components/SearchBar';
 import PostList from './components/PostList';
-import React from 'react';
-
+import Profile from './components/Profile';
+import SearchBar from './components/SearchBar'; 
+import Login from './components/Login'; 
+import axios from 'axios';
 
 function PhoneFrame({ children }) {
   return (
     <div className="phone-frame">
-      <div className="content">
-        {children}
-      </div>
+      <div className="content">{children}</div>
     </div>
   );
 }
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = (value) => {
+    setSearchValue(value);
+  };
+
+  const [currentView, setCurrentView] = useState('postList');
+
+  const handleLogoClick = () => {
+    setCurrentView('postList');
+  };
+
+  const handleProfileClick = () => {
+    setCurrentView('profile');
+  };
+  
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
     <PhoneFrame>
-    <div className="container">
-      <NavBar />
-      <SearchBar />
-      <PostList/>
-    </div>
+      <div className="container">
+        <NavBar
+          onLogoClick={handleLogoClick}
+          onProfileClick={handleProfileClick}
+        />
+        <SearchBar value={searchValue} onSearch={handleSearch} />
+        {authenticated ? (
+          currentView === 'postList' ? (
+            <PostList searchValue={searchValue} />
+          ) : (
+            <Profile />
+          )
+        ) : (
+          <Login onLogin={() => setAuthenticated(true)} />
+        )}
+      </div>
     </PhoneFrame>
   );
-}
-
-export default App;
+        }
+  export default App;  
